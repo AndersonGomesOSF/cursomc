d63551e00e992470485731f74b4c7526c60478bc
Anderson Gomes <andersongomes86@gmail.com>
2020-05-04 16:02:46 -0300

    Projeto foi criado

---
+++ src/test/java/com/osf/treinamento/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osf.treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
