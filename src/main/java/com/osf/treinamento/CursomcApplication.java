80983c505cb71f223b7554fe3ff965029e884500
Anderson Gomes <andersongomes86@gmail.com>
2020-05-05 10:10:49 -0300

    Adicionando cidades e estados

--- src/main/java/com/osf/treinamento/CursomcApplication.java
+++ src/main/java/com/osf/treinamento/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.osf.treinamento.domain.Categoria;
+import com.osf.treinamento.domain.Cidade;
+import com.osf.treinamento.domain.Estado;
 import com.osf.treinamento.domain.Produto;
 import com.osf.treinamento.repositories.CategoriaRepository;
+import com.osf.treinamento.repositories.CidadeRepository;
+import com.osf.treinamento.repositories.EstadoRepository;
 import com.osf.treinamento.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +21,15 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -34,6 +44,13 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -41,9 +58,12 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
-
 }
---
+++ src/main/java/com/osf/treinamento/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.osf.treinamento.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.osf.treinamento.domain.Cidade;
+
+@Repository 
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {	
+	
+}
