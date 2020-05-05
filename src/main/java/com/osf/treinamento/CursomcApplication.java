080f52f6072981e6585b16ceb7b9b138838228ba
Anderson Gomes <andersongomes86@gmail.com>
2020-05-05 11:57:41 -0300

    cliente, tipocliente, telefone e endereço

--- src/main/java/com/osf/treinamento/CursomcApplication.java
+++ src/main/java/com/osf/treinamento/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.osf.treinamento.domain.Categoria;
 import com.osf.treinamento.domain.Cidade;
+import com.osf.treinamento.domain.Cliente;
+import com.osf.treinamento.domain.Endereco;
 import com.osf.treinamento.domain.Estado;
 import com.osf.treinamento.domain.Produto;
+import com.osf.treinamento.domain.enums.TipoCliente;
 import com.osf.treinamento.repositories.CategoriaRepository;
 import com.osf.treinamento.repositories.CidadeRepository;
+import com.osf.treinamento.repositories.ClienteRepository;
+import com.osf.treinamento.repositories.EnderecoRepository;
 import com.osf.treinamento.repositories.EstadoRepository;
 import com.osf.treinamento.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -50,7 +61,15 @@ public class CursomcApplication implements CommandLineRunner {
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "54864795632", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("88888888", "99999999"));
+		
+		Endereco end1 = new Endereco(null, "Rua A", 320, "ap 408", "Jardims","16546484", cli1, c1);
+		Endereco end2 = new Endereco(null, "Rua B", 320, "ap 508", "Aero","16546484", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
+				
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -60,10 +79,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(end1, end2));
 	}
 }
---
+++ src/main/java/com/osf/treinamento/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.osf.treinamento.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.osf.treinamento.domain.Cliente;
+
+@Repository 
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {	
+	
+}
