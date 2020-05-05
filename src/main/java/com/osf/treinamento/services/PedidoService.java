e3020d4a067be6a4cf4f7b71ae312ba51039f240
Anderson Gomes <andersongomes86@gmail.com>
2020-05-05 16:21:41 -0300

    Endpoint /pedidos/{id} disponibilizado update

--- src/main/java/com/osf/treinamento/services/PedidoService.java
+++ src/main/java/com/osf/treinamento/services/PedidoService.java
@@ -5,19 +5,19 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.osf.treinamento.domain.Cliente;
-import com.osf.treinamento.repositories.ClienteRepository;
+import com.osf.treinamento.domain.Pedido;
+import com.osf.treinamento.repositories.PedidoRepository;
 import com.osf.treinamento.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class ClienteService {
+public class PedidoService {
 
 	@Autowired
-	private ClienteRepository repo;
+	private PedidoRepository repo;
 
-	public Cliente find(Integer id) {
-		Optional<Cliente> obj = repo.findById(id);
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 }
