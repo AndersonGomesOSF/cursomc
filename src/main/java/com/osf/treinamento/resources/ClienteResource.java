d8ecb52445ad50e1ccc73bfa8b9804aff73474eb
Anderson Gomes <andersongomes86@gmail.com>
2020-05-06 10:53:14 -0300

    Inserindo uma nova categoria com post

--- src/main/java/com/osf/treinamento/resources/ClienteResource.java
+++ src/main/java/com/osf/treinamento/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.osf.treinamento.resources;
 
+import com.osf.treinamento.domain.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.osf.treinamento.domain.Cliente;
 import com.osf.treinamento.services.ClienteService;
 
 @RestController
--- src/main/java/com/osf/treinamento/services/CategoriaService.java
+++ src/main/java/com/osf/treinamento/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.osf.treinamento.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.osf.treinamento.domain.Categoria;
@@ -12,12 +13,17 @@ import com.osf.treinamento.services.exceptions.ObjectNotFoundException;
 @Service
 public class CategoriaService {
 
-	@Autowired
-	private CategoriaRepository repo;
+    @Autowired
+    private CategoriaRepository repo;
 
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-	}
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
